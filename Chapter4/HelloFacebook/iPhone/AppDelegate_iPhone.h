b36120d4536698695ad0d589c2f015b765bc0e4b
Christopher White <mrchristopher124@gmail.com>
2011-01-03 00:26:14 -0500

    Chapter4: Initial commit

---
+++ Chapter4/HelloFacebook/iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPhone.h
+//  HelloFacebook
+//
+//  Created by Christopher White on 1/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPhone : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
